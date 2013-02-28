4a9281ae2f6071b8f91824da7e6341c97b81b193
thebiologist13 <thebiologist13@gmail.com>
2013-02-28 16:04:34 -0700

    Initial Commit.

---
+++ SuicideSword/src/com/github/suicidesword/SuicideSword.java
@@ -0,0 +1,17 @@
+package com.github.suicidesword;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SuicideSword extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
